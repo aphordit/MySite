875768f3eb9301b46724a632dfcab2988f7367a3
aphordit <bahreinifoad@gmail.com>
2019-07-25 09:17:59 +0430

    First App Settings

--- Web/main/models.py
+++ Web/main/models.py
@@ -1,3 +1,13 @@
+from __future__ import unicode_literals
 from django.db import models
-
-# Create your models here.
+# Create your models here
+# Create your models here
+# Create your models here
+class Main(models.Model):
+    firs_name = models.CharField(max_length=30)
+    last_name = models.TextField()
+    number = models.IntegerField()
+    
+    
+    def __str__(self):
+        return self.firs_name
--- Web/main/views.py
+++ Web/main/views.py
@@ -1,3 +1,9 @@
+from django.shortcuts import get_object_or_404 , redirect
 from django.shortcuts import render
+from .models import Main
+
 
 # Create your views here.
+
+def home (request) :
+    return render (request , 'home.html')
