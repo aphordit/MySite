57c58706a5fd036a6636f2cfd64e5f5640466e98
aphordit <bahreinifoad@gmail.com>
2019-08-13 14:10:40 +0430

    Edit Admin Page

--- Web/settings/urls.py
+++ Web/settings/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^dashboard/settings/add$', views.settings_add, name='settings_add'),
+    url(r'^dashboard/settings/list$', views.settings_show, name='settings_show'),
 ]
--- Web/settings/views.py
+++ Web/settings/views.py
@@ -4,6 +4,21 @@ from .models import Settings
 
 # Create your views here.
 
+def settings_show(request):
+    settings = Settings.objects.all()
+    return render(request, 'Back/settings_list.html', {'settings': settings})
+
+
 def settings_add(request):
 
-    return render(request, 'Back/settings_list.html')
+    if request.method == "POST":
+
+        settings_name = request.POST.get('settings_name')
+        settings_pic_url = request.POST.get('settings_pic_url')
+        settings_head = request.POST.get('settings_head')
+        settings_title = request.POST.get('settings_title')
+        new_settings_item = Settings(settings_name=settings_name, settings_pic_url=settings_pic_url,
+                                     settings_head=settings_head, settings_title=settings_title, settings_pic="")
+        new_settings_item.save()
+        return redirect('settings_show')
+    return render(request, 'Back/admin/settings_add.html')
